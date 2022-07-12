2fc08e9a62eb8fe69761a21563d06b2c67bf6bfe
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-11 22:45:09 -0400

    test to ship and initial class

---
+++ spec/ship_spec.rb
@@ -0,0 +1,36 @@
+require "./lib/ship"
+
+RSpec.describe Ship do
+
+    before :each do 
+        @cruiser = Ship.new("Cruiser", 3)
+    end
+
+    it 'exists' do
+        expect(@cruiser).to be_instance_of(Ship)
+    end
+
+    it 'has attributes' do
+        expect(@cruiser.name).to eq('Cruiser')
+        expect(@cruiser.length).to eq(3)
+        expect(@cruiser.health).to eq(3)
+    end
+
+    it 'can be hit' do
+        expect(@cruiser.health).to eq(3)
+        @cruiser.hit
+        expect(@cruiser.health).to eq(2)
+    end
+
+    it "can be sunk" do
+        expect(@cruiser.sunk?).to be(false)
+        @cruiser.hit
+        @cruiser.hit
+        @cruiser.hit
+        expect(@cruiser.health).to eq(0)
+        expect(@cruiser.sunk?).to be(true)
+    end 
+
+
+
+end
