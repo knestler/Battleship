cacc60c76cbe67716f4dfb142106010e4b4895f5
Gavin Carew <gjcarew@gmail.com>
2022-07-14 08:48:24 -0700

    bug: variable board size not working. Commented out for the time being

--- lib/board.rb
+++ lib/board.rb
@@ -3,22 +3,24 @@ require './lib/cell'
 class Board
   attr_reader :cells
 
+  # default board size = 4
   def initialize(board_size = 4)
-    @board_size = board_size
-    @cells = create_cells
+    # @board_size = board_size
+    # @cells =
   end
 
-  def create_cells
-    letters = "A".."Z"
-    column_labels = letters.to_a.first(@board_size)
-    cell_coords = column_labels.map do |label|
-                    label + (label.ord - 64).to_s
-                  end
-    cells_hash = Hash.new{}
-    cell_coords.each do |coordinate|
-      cells_hash[coordinate] = Cell.new(coordinate)
-    end
-    cells_hash
-  end
+  ## Not yet working... come back to it for Iteration 4
+  # def create_cells
+  #   letters = "A".."Z"
+  #   column_labels = letters.to_a.first(@board_size)
+  #   cell_coords = column_labels.map do |label|
+  #                   label + (label.ord - 64).to_s
+  #                 end
+  #   cells_hash = Hash.new{}
+  #   cell_coords.each do |coordinate|
+  #     cells_hash[coordinate] = Cell.new(coordinate)
+  #   end
+  #   cells_hash
+  # end
 
 end
