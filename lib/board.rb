5d92fbbff3576feb49b87a452450e42f3cca669f
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-14 20:41:30 -0400

    feat: setup of render complete

--- lib/board.rb
+++ lib/board.rb
@@ -48,7 +48,6 @@ class Board
     empty_array = []
     placement_array.each do |coord|
       empty_array << @cells[coord].empty?
-
     end
     empty_array.all?(true)
   end 
@@ -65,8 +64,13 @@ class Board
     end
   end
 
-
-
+  def render(display_ship = false)
+    "  1 2 3 4 \n" +
+    "A" + @cells["A1"].render(display_ship) + @cells["A2"].render(display_ship) + @cells["A3"].render(display_ship) + @cells["A4"].render(display_ship) + "\n" +
+    "B" + @cells["B1"].render(display_ship) + @cells["B2"].render(display_ship) + @cells["B3"].render(display_ship) + @cells["B4"].render(display_ship) + "\n" +
+    "C" + @cells["C1"].render(display_ship) + @cells["C2"].render(display_ship) + @cells["C3"].render(display_ship) + @cells["C4"].render(display_ship) + "\n" +
+    "D" + @cells["D1"].render(display_ship) + @cells["D2"].render(display_ship) + @cells["D3"].render(display_ship) + @cells["D4"].render(display_ship) + "\n"
+  end
 
 
 end
